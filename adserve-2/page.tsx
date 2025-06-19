79a88d6a6c252a1416f92ee005303c1027d2fcf9
LooKes Wong <lookes.wong@kingdomdigital.com.my>
2025-06-19 16:17:16 +0800

    update

---
+++ adserve-2/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Adserve2() {
+    return <div>Adserve 2 page</div>;
+}
---
+++ adserve/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Adserve() {
+    return <div>Adserve 1 page</div>;
+}
